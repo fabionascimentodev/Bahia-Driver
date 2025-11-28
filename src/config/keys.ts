bf8b78ff3f68becf70f794b5073459f4d2a90a8a
Fabio Barbosa Nascimento <141643297+fabionascimentodev@users.noreply.github.com>
2025-11-28 07:58:27 -0300

    Change GOOGLE_MAPS_API_KEY value
    Updated GOOGLE_MAPS_API_KEY to a new value.

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
 // src/config/keys.ts
 
-export const GOOGLE_MAPS_API_KEY = "AIzaSyCZVxpbP1k_mtPt52fPKE2SKapEZ-E-Xpg"; 
+export const GOOGLE_MAPS_API_KEY = "AIzaSyCIV-l5roJ4OE7cmaNsfc3HkSn4jDmA9gA"; 
 // Você deve obter essa chave no Google Cloud Console e habilitar as APIs de Mapas/Geocoding/Directions
